41974ddfa56b2e7cca1f754293a4dee16b129f9d
김신강 <iwan2fly@gmail.com>
2025-04-23 21:24:37 +0900

    sinnaApp

---
+++ module-application/module-sinnaApplication/src/main/java/com/sinnau/SinnaApplication.java
@@ -0,0 +1,11 @@
+package com.sinnau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinnaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SinnaApplication.class, args);
+    }
+} 
