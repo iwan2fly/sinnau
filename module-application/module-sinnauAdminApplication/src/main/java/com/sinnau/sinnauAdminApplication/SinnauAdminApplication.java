f8b3aa024d580a802f0e9cdd5d3794651d5f8490
김신강 <iwan2fly@gmail.com>
2025-04-25 21:47:35 +0900

    sinnauAdminApplication

---
+++ module-application/module-sinnauAdminApplication/src/main/java/com/sinnau/sinnauAdminApplication/SinnauAdminApplication.java
@@ -0,0 +1,11 @@
+package com.sinnau.sinnauAdminApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinnauAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SinnauAdminApplication.class, args);
+    }
+}
