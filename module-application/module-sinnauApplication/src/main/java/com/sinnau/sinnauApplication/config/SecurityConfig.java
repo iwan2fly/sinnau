5ef82fdd8ebf6d812ea45e58be238c8fceaa8162
iwan2fly <iwan2fly@gmail.com>
2025-04-29 16:36:07 +0900

    ㅇㅇ

--- module-application/module-sinnauApplication/src/main/java/com/sinnau/sinnauApplication/config/SecurityConfig.java
+++ module-application/module-sinnauApplication/src/main/java/com/sinnau/sinnauApplication/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.sinnau.sinnauAdminApplication.config;
+package com.sinnau.sinnauApplication.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- module-application/module-sinnauApplication/src/main/java/com/sinnau/sinnauApplication/controller/HomeController.java
+++ module-application/module-sinnauApplication/src/main/java/com/sinnau/sinnauApplication/controller/HomeController.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-  @GetMapping("/")
-  public String home() {
-    return "index";
+  /**
+   * React 애플리케이션의 모든 경로를 index.html로 포워딩하여 클라이언트 사이드 라우팅이 작동하도록 합니다.
+   * 이렇게 하면 React Router가 브라우저에서 경로를 처리할 수 있습니다.
+   */
+  @GetMapping(value = {"/", "/{path:^(?!api|static|css|js|images).*$}/**"})
+  public String forward() {
+    return "forward:/index.html";
   }
 }
