0829536421adc043528f6833fc951965775d8acb
김신강 <iwan2fly@gmail.com>
2025-05-04 21:58:26 +0900

    trae worked

--- module-api/module-authApi/src/main/java/com/sinnau/authapi/model/SignupRequest.java
+++ module-api/module-authApi/src/main/java/com/sinnau/authapi/model/SignupRequest.java
@@ -2,8 +2,9 @@ package com.sinnau.authapi.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
-@Getter @Setter
+@Getter @Setter @ToString
 public class SignupRequest {
     private String name;
     private String password;
