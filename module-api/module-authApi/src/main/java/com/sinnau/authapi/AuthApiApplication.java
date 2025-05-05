5311bae98e54e19c53d0624870c0d42731146795
김신강 <iwan2fly@gmail.com>
2025-05-05 13:35:30 +0900

    trae worked

--- module-api/module-authApi/src/main/java/com/sinnau/authapi/AuthApiApplication.java
+++ module-api/module-authApi/src/main/java/com/sinnau/authapi/AuthApiApplication.java
@@ -3,7 +3,9 @@ package com.sinnau.authapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication( scanBasePackages = {"com.sinnau.authapi", "com.sinnau.domain.user"})
+@SpringBootApplication(scanBasePackages = {"com.sinnau.authapi", "com.sinnau.domain.user"})
+@EnableJpaRepositories(basePackages = "com.sinnau.domain.user.repository")
+@EntityScan(basePackages = "com.sinnau.domain.user.model")
 public class AuthApiApplication {
   public static void main(String[] args) {
     SpringApplication.run(AuthApiApplication.class, args);
