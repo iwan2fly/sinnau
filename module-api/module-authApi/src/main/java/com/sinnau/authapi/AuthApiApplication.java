886f3b3e499b765161b40ac330e4bd42c8e21238
김신강 <iwan2fly@gmail.com>
2025-05-05 13:24:05 +0900

    intelij worked

--- module-api/module-authApi/src/main/java/com/sinnau/authapi/AuthApiApplication.java
+++ module-api/module-authApi/src/main/java/com/sinnau/authapi/AuthApiApplication.java
@@ -3,7 +3,7 @@ package com.sinnau.authapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication( scanBasePackages = {"com.sinnau.authapi", "com.sinnau.domain.user"})
 public class AuthApiApplication {
   public static void main(String[] args) {
     SpringApplication.run(AuthApiApplication.class, args);
