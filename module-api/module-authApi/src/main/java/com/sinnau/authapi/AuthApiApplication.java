4c3095fd4569df9c3070515dcbba9e26f3f6a30c
김신강 <iwan2fly@gmail.com>
2025-05-17 15:33:25 +0900

    intelij worked

--- module-api/module-authApi/src/main/java/com/sinnau/authapi/AuthApiApplication.java
+++ module-api/module-authApi/src/main/java/com/sinnau/authapi/AuthApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.sinnau.authapi", "com.sinnau.domain.user"})
 @EnableJpaRepositories(basePackages = "com.sinnau.domain.user.repository")
-@EntityScan(basePackages = "com.sinnau.domain.user.entity")
+@EntityScan(basePackages = "com.sinnau.domain.user.model.entity")
 public class AuthApiApplication {
   public static void main(String[] args) {
     SpringApplication.run(AuthApiApplication.class, args);
